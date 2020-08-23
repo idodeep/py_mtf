55438356d8546108def56203ab3247fed916e1ba
ido <games@ido.wtf>
2020-08-23 16:14:47 +0300

    refactor

--- mtf.py
+++ mtf.py
@@ -1,11 +1,114 @@
+import sys
+from pathlib import Path
 import matplotlib.pyplot as plt
-from scipy import ndimage as nd
+#from scipy import ndimage as nd
 from matplotlib.patches import Rectangle
 import numpy as np
 from PIL import *
 
-class IMaGE(object):
-    def __init__(self, fit=False):
+
+def luminance(img, out):
+    """
+    Perceived luminance calculation
+    """
+    # Compare with standard luminance: 0.2126*R + 0.7152*G + 0.0722*B
+    lum = 0.299*img[:, :, 0] + 0.589*img[:, :, 1]+0.114*img[:, :, 2]
+
+    plt.figure()
+    plt.title("operator box-area")
+    plt.imshow(lum)
+    imag = Image.fromarray(np.asarray(lum), mode="RGB")
+    plt.show()
+    if out:
+        imag.save(out + "/cut_img.png")
+        plt.savefig(out + "/operator_box_area.png")
+    return lum
+
+
+def ESF(lum, out):
+    """
+    Edge Spread Function calculation
+    """
+    X = lum[100, :]
+    mu = np.sum(X)/X.shape[0]
+    tmp = (X[:] - mu)**2
+    sigma = np.sqrt(np.sum(tmp)/X.shape[0])
+    edge_function = (X[:] - mu)/sigma
+
+    edge_function = edge_function[::3]
+    x = range(0, edge_function.shape[0])
+
+    plt.figure()
+    plt.title(r'ESF')
+    plt.plot(x, edge_function, '-ob')
+    plt.show()
+    if out:
+        plt.savefig(out + "/ESF.png")
+    return edge_function
+
+
+def LSF(esf, out):
+    """
+    Line Spread Function calculation
+    """
+    lsf = esf[:-2] - esf[2:]
+    x = range(0, lsf.shape[0])
+
+    plt.figure()
+    plt.title("LSF")
+    plt.xlabel(r'pixel')
+    plt.ylabel('intensidad')
+    plt.plot(x, lsf, '-or')
+    plt.show()
+    if out:
+        plt.savefig(out + "/LSF.png")
+    return lsf
+
+
+def MTF(lsf, out):
+    """
+    Modulation Transfer Function calculation
+    """
+    mtf = abs(np.fft.fft(lsf))
+    mtf = mtf[:]/np.max(mtf)
+    mtf = mtf[:len(mtf)//2]
+    ix = np.arange(mtf.shape[0]) / (mtf.shape[0])
+    mtf_poly = np.polyfit(ix, mtf, 6)
+    poly = np.poly1d(mtf_poly)
+
+    plt.figure()
+    plt.title("MTF")
+    plt.xlabel(r'Frequency $[cycles/pixel]$')
+    plt.ylabel('mtf')
+    p, = plt.plot(ix, mtf, '-or')
+    ll, = plt.plot(ix, poly(ix))
+    plt.legend([p, ll], ["MTF values", "polynomial fit"])
+    plt.grid()
+    plt.show()
+    if out:
+        plt.savefig(out + "/MTF.png")
+    return mtf
+
+
+def mtf_from_img(image, out):
+    Path(out).mkdir(parents=True, exist_ok=True)
+    lum = luminance(image, out)
+    esf = ESF(lum, out)
+    lsf = LSF(esf, out)
+    mtf = MTF(lsf, out)
+    print(mtf)
+
+
+class ImageAreaSelect(object):
+    def __init__(self, img, out, fit=False):
+        self.img = img
+        self.out = out
+        self.fit = fit
+
+        plt.figure()
+        plt.title("Testing Image")
+        plt.xlabel(r'M')
+        plt.ylabel(r'N')
         self.ax = plt.gca()
         self.rect = Rectangle((0, 0), 1, 1, antialiased=True, color='b',
                               linestyle='solid', lw=1.2)
@@ -14,121 +117,57 @@ class IMaGE(object):
         self.y0 = None
         self.x1 = None
         self.y1 = None
-        self.fit = fit
 
-        self.key = False
-        self.count = 0
+        self.draw_rect = False
 
         self.ax.add_patch(self.rect)
         self.ax.figure.canvas.mpl_connect('button_press_event', self.on_press)
+        self.ax.figure.canvas.mpl_connect('button_release_event', self.on_release)
         self.ax.figure.canvas.mpl_connect('motion_notify_event', self.on_motion)
 
+        plt.imshow(self.img, cmap="gray")
+        plt.show()
+
     def on_press(self, event):
-        self.count += 1
-        self.key = True if self.key % 2 == 0 else False
-
-        if self.key % 2 == 0:
-            self.x = range(int(self.x0), int(self.x1))
-            self.calc(im, x0=self.x0, y0=self.y0, x1=self.x1, y1=self.y1)
-
-        self.x0 = event.xdata
-        self.y0 = event.ydata
-
-    def calc(self, image, **args):
-        self.cut(image, **args)
-        self.ESF()
-        self.LSF()
-        self.MTF()
+        self.draw_rect = True
+        self.x0 = int(event.xdata)
+        self.y0 = int(event.ydata)
 
     def on_motion(self, event):
-        if self.key:
-            self.x1 = event.xdata
-            self.y1 = event.ydata
-            self.rect.set_width(self.x1 - self.x0)
-            self.rect.set_height(self.y1 - self.y0)
-            self.rect.set_xy((self.x0, self.y0))
-            self.ax.figure.canvas.draw()
+        if self.draw_rect:
+            if self.x1 != int(event.xdata) or self.y1 != int(event.ydata):
+                self.x1 = int(event.xdata)
+                self.y1 = int(event.ydata)
+                self.rect.set_width(self.x1 - self.x0)
+                self.rect.set_height(self.y1 - self.y0)
+                self.rect.set_xy((self.x0, self.y0))
+                self.ax.figure.canvas.draw()
 
-    def cut(self, image, **args):
-        for i in args.keys():
-            print("{} : {}".format(i, args[i]))
+    def on_release(self, event):
+        self.draw_rect = False
+        self.x1 = int(event.xdata)
+        self.y1 = int(event.ydata)
+        self.calc()
 
-        M = image[int(args["y0"]):int(args["y1"]), int(args["x0"]):int(args["x1"])]
-        self.M_out = 0.299*M[:, :, 0] + 0.589*M[:, :, 1]+0.114*M[:, :, 2]
-        
-        plt.figure()
-        plt.title("operator box-area")
-        plt.imshow(self.M_out)
-        name = "box_selection_{}.png".format(self.count)
-        imag = Image.fromarray(np.asarray(self.M_out), mode = "RGB")
-        imag.save(".\out\cut_img.png")
-        plt.show()
-        plt.savefig("./out/operator_box_area.png")
+    def calc(self):
+        if self.x1 == self.x0 or self.y1 == self.y0:
+            return
+        print('crop(x,y):', self.x0, self.x1, self.y0, self.y1)
+        img = self.img[self.y0:self.y1, self.x0:self.x1]
+        mtf_from_img(img, self.out + "/" + str(self.x0) + "_" + str(self.x1) + "_" + str(self.y0) + "_" + str(self.y1))
 
-    def ESF(self):
-        """
-        Edge Spread Function calculation
-        """
-
-        self.X = self.M_out[100,:]
-        mu = np.sum(self.X)/self.X.shape[0]
-        tmp = (self.X[:] - mu)**2
-        sigma = np.sqrt(np.sum(tmp)/self.X.shape[0])
-        self.edge_function = (self.X[:] - mu)/sigma
-        
-        self.edge_function = self.edge_function[::3]
-        x = range(0,self.edge_function.shape[0])
-
-        plt.figure()
-        plt.title(r'ESF')
-        plt.plot(x, self.edge_function, '-ob')
-        plt.show()
-        plt.savefig("./out/ESF.png")
-
-    def LSF(self):
-        """
-        Line Spread Function calculation
-        """
-        self.lsf = self.edge_function[:-2] - self.edge_function[2:]
-        x = range(0,self.lsf.shape[0])
-        
-        plt.figure()
-        plt.title("LSF")
-        plt.xlabel(r'pixel')
-        plt.ylabel('intensidad')
-        plt.plot(x,self.lsf, '-or')
-        plt.show()
-        plt.savefig("./out/LSF.png")
-
-    def MTF(self):
-        """
-        Modulation Transfer Function calculation
-        """
-        self.mtf = abs(np.fft.fft(self.lsf))
-        self.mtf = self.mtf[:]/np.max(self.mtf)
-        self.mtf = self.mtf[:len(self.mtf)//2]
-        ix = np.arange(self.mtf.shape[0]) / (self.mtf.shape[0])
-        mtf_poly =  np.polyfit(ix, self.mtf, 6)
-        poly = np.poly1d(mtf_poly)
-        
-        plt.figure()
-        plt.title("MTF")
-        plt.xlabel(r'Frecuency $[cycles/pixel]$')
-        plt.ylabel('mtf')
-        p, = plt.plot(ix, self.mtf, '-or')
-        ll, = plt.plot(ix, poly(ix))
-        plt.legend([p, ll], ["MTF values", "polynomial fit"])
-        plt.grid()
-        plt.show()
-        plt.savefig("./out/MTF.png")
 
 if __name__ == "__main__":
-    plt.figure()
-    plt.title("Testing Image")
-    plt.xlabel(r'M')
-    plt.ylabel(r'N')
-    im = plt.imread("./images/prueba.png")
-    a = IMaGE(fit=True)
+    print("Usage: mtf.py <image-file> <output-folder>")
+    # set defaults
+    if len(sys.argv) > 1:
+        im_file = sys.argv[1]
+    else:
+        im_file = "./images/prueba.png"
+    if len(sys.argv) > 2:
+        out_folder = sys.argv[2]
+    else:
+        out_folder = "./out"
 
-    plt.imshow(im, cmap="gray")
-    plt.show()
+    im = plt.imread(im_file)
+    ImageAreaSelect(im, out_folder, fit=True)
